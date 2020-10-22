74b08c86ee126e2dfdd0a471d421deeedb647da4
UsamaLion <ulion269@gmail.com>
2020-10-22 13:58:16 +0500

    Learning React

---
+++ src/test-u.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Legneds(props) {
+    return(
+        <div>
+            <h1>Legneds</h1>
+            <p>Once a legneds said that if you want to be a human <br />
+            you need act like human not look like human </p>
+            
+            <div>
+            <p>animals name {props.anim}</p>
+            <p>animals name {props.anim}</p>
+            </div>
+        </div>
+        
+    );
+}
+export default Legneds;
